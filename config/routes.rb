eee1db10cd6713bf18dc687be31c3cb967f0b9d6
Logan Yu <yu.logan@gmail.com>
2017-04-20 10:53:47 -0400

    added slash messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboard#index'
+
+  scope '/api' do
+    scope '/slack_commands' do
+      post '/' => 'slack_commands#create'
+    end
+  end
 end
